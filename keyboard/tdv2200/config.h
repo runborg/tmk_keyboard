a829ed0eefad749385af6e062a16874c5343870b
runborg <runar@13xl.com>
2015-07-18 18:52:50 +0200

    Modified command key combination to Shift+ctrl+alt
     gui and shift is nonexistent on keyboard at the moment

--- keyboard/tdv2200/config.h
+++ keyboard/tdv2200/config.h
@@ -45,8 +45,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key combination for command */
 #define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_LCTRL) | MOD_BIT(KC_LALT) | MOD_BIT(KC_LGUI)) || \
-    keyboard_report->mods == (MOD_BIT(KC_LALT) | MOD_BIT(KC_RALT)) \
+    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_LCTRL) | MOD_BIT(KC_LALT)) || \
+    keyboard_report->mods == (MOD_BIT(KC_F13) | MOD_BIT(KC_F14)) \
 )
 
 #endif
